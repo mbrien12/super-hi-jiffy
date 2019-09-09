bf7e98ffb880c75e5808d74c4215f2476665a913
Michelle Brien <michelle.brien12@gmail.com>
2019-09-09 23:04:07 +0100

    setup main components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import loader from "./images/loader.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+);
+
+const UserHint = ({ loading, hintText }) => (
+  <div className="user-hint">
+    {loading ? (
+      <img className="block mx-auto" src={loader} alt="loading state" />
+    ) : (
+      hintText
+    )}
+  </div>
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      hintText: ''
+    };
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value,
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : ''
+    }));
+  };
+
+  handleKeyPress = event => {
+    const { value } = event.target;
+    if (value.length > 2 && event.key === "Enter") {
+      alert(`search for ${value}`);
+    }
+  };
+
+  render() {
+    // same as searchTerm = this.state.searchTerm
+    const { searchTerm } = this.state;
+    return (
+      <div className="page">
+        <Header />
+        <div className="search grid">
+          <input
+            className="input grid-item"
+            placeholder="Type something"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            value={searchTerm}
+          />
+        </div>
+        {/*Pass user hint everyhing in state using a spread */}
+        <UserHint {...this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
