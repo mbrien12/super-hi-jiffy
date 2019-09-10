bd02e5da206ff320901abe52f983cdefa089ff64
Michelle Brien <michelle.brien12@gmail.com>
2019-09-10 23:07:30 +0100

    adds gif styles

--- src/Gif.js
+++ src/Gif.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 
 class Gif extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
   render() {
+    const {loaded} = this.state
     const {images} = this.props
     return (
       <video
-        className="grid-item video"
+        className={`grid-item video ${loaded && 'loaded'}`}
         autoPlay
         loop
         src={images.original.mp4}
+        onLoadedData={() => this.setState({loaded: true})}
       />
     );
   }
