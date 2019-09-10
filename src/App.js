8685ab6b48e020c9149a67dad7b79dc530b1f1df
Michelle Brien <michelle.brien12@gmail.com>
2019-09-10 22:37:33 +0100

    created Gif component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import Gif from "./Gif"
 import loader from "./images/loader.svg";
+
+const randomChoice = arr => {
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+};
 
 const Header = () => (
   <div className="header grid">
@@ -22,33 +28,54 @@ class App extends Component {
     super(props);
     this.state = {
       searchTerm: "",
-      hintText: ''
+      hintText: "",
+      gif: null,
+      gifs: []
     };
   }
+
+  searchGiphy = async searchTerm => {
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=3n9MYXQNqASCZodNQ6fyqAtVgFLWop7I&q=${searchTerm}&limit=100&offset=0&rating=G&lang=en`
+      );
+      const { data } = await response.json();
+      const randomGif = randomChoice(data);
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gif: randomGif,
+        gifs: [...prevState.gifs, randomGif]
+      }));
+    } catch (error) {}
+  };
 
   handleChange = event => {
     const { value } = event.target;
     this.setState((prevState, props) => ({
       ...prevState,
       searchTerm: value,
-      hintText: value.length > 2 ? `Hit enter to search ${value}` : ''
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : ""
     }));
   };
 
   handleKeyPress = event => {
     const { value } = event.target;
     if (value.length > 2 && event.key === "Enter") {
-      alert(`search for ${value}`);
+      this.searchGiphy(value);
     }
   };
 
   render() {
     // same as searchTerm = this.state.searchTerm
-    const { searchTerm } = this.state;
+    const { searchTerm, gifs } = this.state;
     return (
       <div className="page">
         <Header />
         <div className="search grid">
+          {gifs.map(gif => (
+            <Gif {...gif} />
+          ))}
           <input
             className="input grid-item"
             placeholder="Type something"
